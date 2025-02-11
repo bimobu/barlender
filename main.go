68885cd1d29b273ac2e4973dde22f62c84b77060
Fabian Gadau <fabian.gadau@gmail.com>
2025-02-11 22:03:24 +0100

    feat: styling works

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"barlender/components/layout"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Static("/static", "./static")
+
 	r.GET("/", func(c *gin.Context) {
-		component := hello("Fabi")
+		component := layout.Template("Simon")
 		component.Render(c.Request.Context(), c.Writer)
 	})
 
